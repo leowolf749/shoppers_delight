f6dace7211c986636a5ac5b4025e791232a3d92c
leowolf749 <leowolf749@gmail.com>
2017-01-02 18:46:25 -0500

    Added images and styled the fonts. Still need to get the subtotal, tax, and totals to calculate and display when items are added to the cart.

--- app.js
+++ app.js
@@ -6,38 +6,47 @@ window.addEventListener('load', function() {
 let masks = [
     {   
         name: 'Keaton Mask',
+        image: 'http://www.zeldadungeon.net/wiki/images/thumb/e/e3/Keaton-Mask-Model.png/100px-Keaton-Mask-Model.png',
         price: 10 + ' rupees', 
     },
     {
         name: 'Skull Mask',
+        image: 'http://www.zeldadungeon.net/Zelda05/Masks/Skull.png',
         price: 20 + ' rupees',
     },
     {
         name: 'Spooky Mask',
+        image: 'http://www.zeldadungeon.net/Zelda05/Masks/Spooky.png',
         price: 30 + ' rupees',
     },
     {
         name: 'Bunny Hood',
+        image: 'http://www.zeldadungeon.net/wiki/images/thumb/f/fd/Bunny-Hood.png/100px-Bunny-Hood.png',
         price: 50 + ' rupees',
     },
     {
         name: 'Mask of Truth',
+        image: 'http://www.zeldadungeon.net/wiki/images/thumb/2/29/Mask-of-Truth.png/100px-Mask-of-Truth.png',
         price: 100 + ' rupees',
     },
         {
         name: 'Gerudo Mask',
+        image: 'http://www.zeldadungeon.net/Zelda05/Masks/Gerudo.png',
         price: 200 + ' rupees',
     },
         {
         name: 'Goron Mask',
+        image: 'http://www.zeldadungeon.net/wiki/images/thumb/1/13/Goron-Mask.png/100px-Goron-Mask.png',
         price: 200 + ' rupees',
     },
         {
         name: 'Zora Mask',
+        image: 'http://www.zeldadungeon.net/wiki/images/thumb/1/19/Zora-Mask.png/100px-Zora-Mask.png',
         price: 200 + ' rupees',
     },
         {
         name: 'Majoras Mask',
+        image: 'http://www.zeldadungeon.net/wiki/images/thumb/e/ed/Majora%27sMask-Artwork.png/250px-Majora%27sMask-Artwork.png',
         price: 10000 + ' rupees',
     },
 
@@ -79,7 +88,7 @@ function showMasks(masks) {
                 subTotal = 'Subtotal: ' + masks[i].price;
                 console.log(subTotal);
         });
-        
+            
     }
 }
 
