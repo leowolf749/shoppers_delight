5884736351e2aca38699e9ce0fa8e2027f2e7c0e
leowolf749 <leowolf749@gmail.com>
2017-01-03 13:37:23 -0500

    Fixed the showMasks function and moved the 'for loop' to run when the page loads.

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
-window.addEventListener('load', function() {
-    showMasks(masks);
-});
-
 
 let masks = [
     {   
         name: 'Keaton Mask',
         image: 'http://www.zeldadungeon.net/wiki/images/thumb/e/e3/Keaton-Mask-Model.png/100px-Keaton-Mask-Model.png',
-        price: 10 + ' rupees', 
+        price: 10 + ' Rupees', 
     },
     {
         name: 'Skull Mask',
         image: 'http://www.zeldadungeon.net/Zelda05/Masks/Skull.png',
-        price: 20 + ' rupees',
+        price: 20 + ' Rupees',
     },
     {
         name: 'Spooky Mask',
         image: 'http://www.zeldadungeon.net/Zelda05/Masks/Spooky.png',
-        price: 30 + ' rupees',
+        price: 30 + ' Rupees',
     },
     {
         name: 'Bunny Hood',
         image: 'http://www.zeldadungeon.net/wiki/images/thumb/f/fd/Bunny-Hood.png/100px-Bunny-Hood.png',
-        price: 50 + ' rupees',
+        price: 50 + ' Rupees',
     },
     {
         name: 'Mask of Truth',
         image: 'http://www.zeldadungeon.net/wiki/images/thumb/2/29/Mask-of-Truth.png/100px-Mask-of-Truth.png',
-        price: 100 + ' rupees',
+        price: 100 + ' Rupees',
     },
         {
         name: 'Gerudo Mask',
         image: 'http://www.zeldadungeon.net/Zelda05/Masks/Gerudo.png',
-        price: 200 + ' rupees',
+        price: 200 + ' Rupees',
     },
         {
         name: 'Goron Mask',
         image: 'http://www.zeldadungeon.net/wiki/images/thumb/1/13/Goron-Mask.png/100px-Goron-Mask.png',
-        price: 200 + ' rupees',
+        price: 200 + ' Rupees',
     },
         {
         name: 'Zora Mask',
         image: 'http://www.zeldadungeon.net/wiki/images/thumb/1/19/Zora-Mask.png/100px-Zora-Mask.png',
-        price: 200 + ' rupees',
+        price: 200 + ' Rupees',
     },
         {
         name: 'Majoras Mask',
         image: 'http://www.zeldadungeon.net/wiki/images/thumb/e/ed/Majora%27sMask-Artwork.png/250px-Majora%27sMask-Artwork.png',
-        price: 10000 + ' rupees',
+        price: 10000 + ' Rupees',
     },
 
 ];
 
+let cart = [];
+
+window.addEventListener('load', function() {
+    for (let i = 0; i < masks.length; i++) {
+        showMasks(masks[i]);
+    }
+});
+
 
 
 function showMasks(masks) {
-    let parent = document.querySelector('#masks');
-    for (let i = 0; i < masks.length; i++) {
-        
-        let mask = document.createElement('li');
+    let parent = document.querySelector('#masks');   
+    let mask = document.createElement('li');
+    let maskTemplate = document.querySelector('#mask-template');
 
-        mask.innerHTML = Mustache.render(
-            document.querySelector('#mask-template').innerHTML,
-            { image: masks[i].image,  
-              name: masks[i].name,
-              price: masks[i].price,
+        mask.innerHTML = Mustache.render(maskTemplate.innerHTML,
+            { image: masks.image,  
+              name: masks.name,
+              price: masks.price,
             }
         );
     parent.appendChild(mask);
@@ -72,24 +74,24 @@ function showMasks(masks) {
     let cart = document.querySelector('#cart');
     let addButton = mask.querySelector('.add');
         addButton.addEventListener('click', function() {
-            console.log('added ' + masks[i].name + ' to cart');
+            console.log('added ' + masks.name + ' to cart');
             
             let cartItem = document.createElement('li');
 
             cartItem.innerHTML = Mustache.render(
                 document.querySelector('#cart-template').innerHTML,
-                { name: masks[i].name,
-                  price: masks[i].price,  
+                { name: masks.name,
+                  price: masks.price,  
                 }
             );
             cart.appendChild(cartItem);
 
             let subTotal = document.querySelector('.subtotal');
-                subTotal = 'Subtotal: ' + masks[i].price;
+                subTotal = 'Subtotal: ' + masks.price;
                 console.log(subTotal);
         });
             
-    }
+    
 }
 
 
